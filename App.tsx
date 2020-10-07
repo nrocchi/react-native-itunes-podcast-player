f6f29d5771b50302e8a8686146b6d65db60fdd56
Nicolas <nicolas.rocchi.ozon@gmail.com>
2020-10-07 12:47:10 +0200

    fix app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,28 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {Box, UtilityThemeProvider} from 'react-native-design-utility';
-import {ApolloProvider} from '@apollo/react-hooks';
-import TrackPlayer from 'react-native-track-player';
-import SplashScreen from 'react-native-splash-screen';
+import 'react-native-gesture-handler'
+import * as React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {Box, UtilityThemeProvider} from 'react-native-design-utility'
+import {ApolloProvider} from '@apollo/react-hooks'
+import TrackPlayer from 'react-native-track-player'
+import SplashScreen from 'react-native-splash-screen'
 
-import {theme} from './src/constants/theme';
-import MainStackNavigator from './src/navigators/MainStackNavigator';
-import {client} from './src/graphql/client';
-import {ActivityIndicator} from 'react-native';
-import {PlayerContextProvider} from './src/context/PlayerContext';
-import {DBProvider} from './src/context/DBContext';
+import {ActivityIndicator} from 'react-native'
+import {theme} from './src/constants/theme'
+import MainStackNavigator from './src/navigators/MainStackNavigator'
+import {client} from './src/graphql/client'
+import {PlayerContextProvider} from './src/context/PlayerContext'
+import {DBProvider} from './src/context/DBContext'
 
 const App = () => {
-  const [isReady, setIsReady] = React.useState<boolean>(false);
+  const [isReady, setIsReady] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    SplashScreen.hide();
-  }, []);
+    SplashScreen.hide()
+  }, [])
 
   React.useEffect(() => {
     TrackPlayer.setupPlayer().then(() => {
-      console.log('TrackPlayer is setup');
+      console.log('TrackPlayer is setup')
 
       TrackPlayer.updateOptions({
         capabilities: [
@@ -33,11 +33,11 @@ const App = () => {
           TrackPlayer.CAPABILITY_JUMP_BACKWARD,
         ],
         jumpInterval: 30,
-      });
+      })
 
-      setIsReady(true);
-    });
-  }, []);
+      setIsReady(true)
+    })
+  }, [])
   return (
     <UtilityThemeProvider theme={theme}>
       <DBProvider>
@@ -56,7 +56,7 @@ const App = () => {
         </ApolloProvider>
       </DBProvider>
     </UtilityThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
