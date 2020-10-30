25668e6a028c3ae1827cf2fda119e81acdcb71a9
Nicolas <nicolas.rocchi.ozon@gmail.com>
2020-10-30 13:56:58 +0100

    Change heroku app uri

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import ApolloClient from 'apollo-boost'
 
 export const client = new ApolloClient({
-  uri: 'https://frozen-river-77426.herokuapp.com/query',
+  uri: 'https://itunes-podcast-api.herokuapp.com/query',
   // uri: 'http://localhost:8080/query',
 })
